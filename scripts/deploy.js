1da2874cbfef50a3e3e56fe01357c92b189c02da
cmark0v <cmark0v@undef>
2021-01-26 20:31:17 +0900

    better publish script, writing js tests

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,19 +2,26 @@ const fs = require("fs");
 const hre = require("hardhat");
 
 async function main() {
-  const Token = await hre.ethers.getContractFactory("MockToken");
-  const token = await Token.deploy("DAI token", "DAI");
-  await token.deployed();
+  const Dai = await hre.ethers.getContractFactory("MockToken");
+  const dai = await Dai.deploy("DAI token", "DAI");
+  await dai.deployed();
 
-  console.log("token mock deployed to:", token.address);
-  publish(token, "MockToken", 'mocks');
+  console.log("DAI token mock deployed to:", dai.address);
+  publish(dai, "MockToken", "mocks", "DAI");
+
+  const Weth = await hre.ethers.getContractFactory("MockToken");
+  const weth = await Weth.deploy("WETH token", "WETH");
+  await weth.deployed();
+
+  console.log("WETH token mock deployed to:", weth.address);
+  publish(weth, "MockToken", "mocks", "WETH");
 
   const Diamondhands = await hre.ethers.getContractFactory("Diamondhands");
-  const diamondhands = await Diamondhands.deploy(token.address, 3600);
+  const diamondhands = await Diamondhands.deploy(dai.address, 3600);
   await diamondhands.deployed();
 
   console.log("Diamondhands deployed to:", diamondhands.address);
-  publish(diamondhands, "Diamondhands",'');
+  publish(diamondhands, "Diamondhands");
 }
 
 main()
@@ -24,9 +31,14 @@ main()
     process.exit(1);
   });
 
-function publish(contractOb, contractName,path) {
+function publish(contractOb, contractName, path, publishName) {
   const address = JSON.stringify(contractOb.address, null, 2);
-
+  if (publishName == undefined || publishName == "") {
+    publishName = contractName;
+  }
+  if (path == undefined) {
+    path = "";
+  }
   const contractArtifactFile = `../artifacts/contracts/${path}/${contractName}.sol/${contractName}.json`;
   const contractArtifacts = require(contractArtifactFile);
   const abi = JSON.stringify(contractArtifacts.abi, null, 2);
@@ -35,10 +47,10 @@ function publish(contractOb, contractName,path) {
   if (!fs.existsSync(folder)) {
     fs.mkdir(folder, console.log);
   }
-  const abiFileName = `${folder}${contractName}.abi.js`;
+  const abiFileName = `${folder}${publishName}.abi.js`;
   //fs.writeFile(abiFileName, "", console.log);
   fs.writeFileSync(abiFileName, `module.exports = ${abi};`, console.log);
-  const addressFileName = `${folder}${contractName}.address.js`;
+  const addressFileName = `${folder}${publishName}.address.js`;
   fs.writeFileSync(
     addressFileName,
     `module.exports = ${address};`,
--- test/test_diamondhands.js
+++ test/test_diamondhands.js
@@ -1,23 +1,35 @@
 const { expect } = require("chai");
 
-tokenAddress = require("../artifacts/published/MockToken.address.js");
-tokenAbi = require("../artifacts/published/MockToken.abi.js");
+DAIAddress = require("../artifacts/published/DAI.address.js");
+DAIAbi = require("../artifacts/published/DAI.abi.js");
+
+dhAddress = require("../artifacts/published/Diamondhands.address.js");
+dhAbi = require("../artifacts/published/Diamondhands.abi.js");
 
 describe("test Diamondhands", function () {
-  let token;
+  let DAI;
   let accounts;
+  let dh;
   before(async () => {
-    token = new ethers.Contract(
-      tokenAddress,
-      tokenAbi,
+    DAI = new ethers.Contract(
+      DAIAddress,
+      DAIAbi,
       ethers.provider.getSigner(0)
     );
     accounts = await ethers.provider.listAccounts();
+    dh = new ethers.Contract(dhAddress, dhAbi, ethers.provider.getSigner(0));
   });
-  it("test mock token", async function () {
-    bal1 = await token.balanceOf(accounts[1]);
-    await token.transfer(accounts[1], 1000);
-    bal = await token.balanceOf(accounts[1]);
-    expect(bal-bal1).to.equal(1000);
+  it("test mock DAI", async function () {
+    bal1 = await DAI.balanceOf(accounts[1]);
+    await DAI.transfer(accounts[1], 1000);
+    bal = await DAI.balanceOf(accounts[1]);
+    expect(bal.sub(bal1)).to.equal(1000);
+  });
+  it("deposit to Diamondhands", async function () {
+    bal1 = await DAI.balanceOf(dh.address);
+    await DAI.approve(dh.address, 1000);
+    await dh.deposit(1000);
+    bal = await DAI.balanceOf(dh.address);
+    expect(bal.sub(bal1)).to.equal(1000);
   });
 });
