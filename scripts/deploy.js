91b755007f9c4f48a97a963b331e1c541227ad24
cmark0v <cmark0v@undef>
2021-01-26 17:59:10 +0900

    script to publish address and abi to file for testing

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,25 +1,44 @@
+const fs = require("fs");
 const hre = require("hardhat");
 
 async function main() {
-
   const Token = await hre.ethers.getContractFactory("MockToken");
-  const token = await Token.deploy("DAI token","DAI");
+  const token = await Token.deploy("DAI token", "DAI");
   await token.deployed();
+
   console.log("token mock deployed to:", token.address);
+  publish(token, "MockToken");
 
   const Diamondhands = await hre.ethers.getContractFactory("Diamondhands");
-  const diamondhands = await Diamondhands.deploy(token.address,3600);
-
+  const diamondhands = await Diamondhands.deploy(token.address, 3600);
   await diamondhands.deployed();
+
   console.log("Diamondhands deployed to:", diamondhands.address);
-
-
+  publish(diamondhands, "Diamondhands");
 }
 
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
 
+function publish(contractOb, contractName) {
+  const abi = JSON.stringify(contractOb.interface, null, 2);
+  const address = JSON.stringify(contractOb.address, null, 2);
+
+  const folder = "artifacts/published/";
+  if (!fs.existsSync(folder)) {
+    fs.mkdir(folder, console.log);
+  }
+  const abiFileName = `${folder}${contractName}.abi.js`;
+  //fs.writeFile(abiFileName, "", console.log);
+  fs.writeFileSync(abiFileName, `module.exports = ${abi};`, console.log);
+  const addressFileName = `${folder}${contractName}.address.js`;
+  fs.writeFileSync(
+    addressFileName,
+    `module.exports = ${address};`,
+    console.log
+  );
+}
