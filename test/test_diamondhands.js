6b50080297c7b9bfc6e914255902fa017c123a14
cmark0v <cmark0v@undef>
2021-01-27 04:08:04 +0900

    add prettierrc

--- test/test_diamondhands.js
+++ test/test_diamondhands.js
@@ -1,15 +1,15 @@
-const { expect } = require("chai");
+const { expect } = require('chai');
 
-DAIAddress = require("../artifacts/published/DAI.address.js");
-DAIAbi = require("../artifacts/published/DAI.abi.js");
+DAIAddress = require('../artifacts/published/DAI.address.js');
+DAIAbi = require('../artifacts/published/DAI.abi.js');
 
-MCKAddress = require("../artifacts/published/MCK.address.js");
-MCKAbi = require("../artifacts/published/MCK.abi.js");
+MCKAddress = require('../artifacts/published/MCK.address.js');
+MCKAbi = require('../artifacts/published/MCK.abi.js');
 
-dhAddress = require("../artifacts/published/Diamondhands.address.js");
-dhAbi = require("../artifacts/published/Diamondhands.abi.js");
+dhAddress = require('../artifacts/published/Diamondhands.address.js');
+dhAbi = require('../artifacts/published/Diamondhands.abi.js');
 
-describe("test Diamondhands", function () {
+describe('test Diamondhands', function () {
   let DAI;
   let accounts;
   let dh;
@@ -20,20 +20,20 @@ describe("test Diamondhands", function () {
     accounts = await ethers.provider.listAccounts();
     dh = new ethers.Contract(dhAddress, dhAbi, ethers.provider.getSigner(0));
   });
-  it("test mock DAI", async function () {
+  it('test mock DAI', async function () {
     bal1 = await DAI.balanceOf(accounts[1]);
     await DAI.transfer(accounts[1], 1000);
     bal = await DAI.balanceOf(accounts[1]);
     expect(bal.sub(bal1)).to.equal(1000);
   });
-  it("deposit to Diamondhands", async function () {
+  it('deposit to Diamondhands', async function () {
     bal1 = await DAI.balanceOf(dh.address);
     await DAI.approve(dh.address, 1000);
     await dh.deposit(1000);
     bal = await DAI.balanceOf(dh.address);
     expect(bal.sub(bal1)).to.equal(1000);
   });
-  it("test withdrawLost", async function () {
+  it('test withdrawLost', async function () {
     await MCK.transfer(dh.address, 1000);
     bal1 = await MCK.balanceOf(accounts[0]);
     await dh.withdrawLost(MCK.address);
