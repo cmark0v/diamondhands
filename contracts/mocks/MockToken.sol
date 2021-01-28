6f5b14655eb5a76cc77f4f930f6595e5d7968411
cmark0v <cmark0v@undef>
2021-01-28 15:16:10 +0900

    ran prettier

--- contracts/mocks/MockToken.sol
+++ contracts/mocks/MockToken.sol
@@ -3,10 +3,7 @@ pragma solidity ^0.6.7;
 import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract MockToken is ERC20 {
-    constructor(
-        string memory name,
-        string memory ticker
-    ) public ERC20(name, ticker) {
+    constructor(string memory name, string memory ticker) public ERC20(name, ticker) {
         _mint(msg.sender, 1000000 * (10**uint256(decimals())));
     }
 }
